9eb15113e232342586cc480363100817b937f6c6
heqingpan <heqingpan@126.com>
2015-05-25 14:02:05 +0800

    初始化kylin-dorm模块

---
+++ kylin-dorm/src/main/java/org/kylin/dorm/App.java
@@ -0,0 +1,13 @@
+package org.kylin.dorm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
