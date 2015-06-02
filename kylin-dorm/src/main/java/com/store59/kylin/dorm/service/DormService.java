1546d54b53544341d506d750d1644bce7ac2ddba
heqingpan <heqingpan@126.com>
2015-06-02 14:14:39 +0800

    kylin-dorm:添加dorm service 实现getDorm updateDorm方法

---
+++ kylin-dorm/src/main/java/com/store59/kylin/dorm/service/DormService.java
@@ -0,0 +1,21 @@
+package com.store59.kylin.dorm.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.store59.kylin.dorm.data.dao.DormDao;
+import com.store59.kylin.dorm.data.model.Dorm;
+
+@Service
+public class DormService {
+	@Autowired
+	private DormDao dormDao;
+
+	public Dorm getDorm(Integer dormId) {
+		return dormDao.selectByPrimaryKey(dormId);
+	}
+
+	public Boolean updateDorm(Dorm dorm) {
+		return dormDao.updateByPrimaryKeySelective(dorm);
+	}
+}
