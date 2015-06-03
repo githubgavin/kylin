ec38fdf0e2f44d0310b132967bf28664c10076d0
heqingpan <heqingpan@126.com>
2015-06-03 11:38:30 +0800

    kylin-dormapi:增加确认要操作的楼主与token的楼主是否匹配

--- kylin-dormapi/src/main/java/com/store59/kylin/dormapi/logic/DormLogic.java
+++ kylin-dormapi/src/main/java/com/store59/kylin/dormapi/logic/DormLogic.java
@@ -11,8 +11,8 @@ import com.store59.kylin.dormapi.exception.ServiceException;
 public class DormLogic {
 	@Autowired
 	private DormService dormService;
-	
-	public Dorm getDormByUid(Integer uid){
+
+	public Dorm getDormByUid(Integer uid) {
 		Dorm dorm = dormService.getDormByUid(uid);
 		if (dorm == null) {
 			throw new ServiceException(1000, "不存在该楼主");
@@ -26,6 +26,21 @@ public class DormLogic {
 			throw new ServiceException(-1);
 		}
 		return dorm;
+	}
+
+	/**
+	 * 确认楼主与token是否匹配
+	 * 
+	 * @param dormId
+	 * @param token
+	 * @return
+	 */
+	public Boolean checkDormId(Integer dormId, UserToken token) {
+		if (dormId == null || token == null
+				|| !dormId.equals(token.getDormId())) {
+			throw new ServiceException(1003, "不匹配的楼主");
+		}
+		return true;
 	}
 
 }
