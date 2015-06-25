d97dbc696c3899d748609a882096b7251405ba36
heqingpan <heqingpan@126.com>
2015-06-25 14:56:20 +0800

    实现GET /order/info接口

--- kylin-api/src/main/java/com/store59/kylin/api/controller/OrderController.java
+++ kylin-api/src/main/java/com/store59/kylin/api/controller/OrderController.java
@@ -12,13 +12,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.store59.kylin.api.viewmodel.Result;
+import com.store59.kylin.order.data.model.Order;
 import com.store59.kylin.order.data.model.Orderfood;
+import com.store59.kylin.order.data.service.OrderService;
 import com.store59.kylin.order.data.service.OrderfoodService;
 
 @RestController
 public class OrderController {
 	@Autowired
 	private OrderfoodService orderfoodService;
+	@Autowired
+	private OrderService orderService;
 	
 	@RequestMapping(value = "/order/foods", method = RequestMethod.GET)
 	public Object getFoodList(HttpServletRequest request, Long order_id) {
@@ -27,6 +31,14 @@ public class OrderController {
 		data.put("foods", foods);
 		Result result = new Result();
 		result.setData(data);
+		return result;
+	}
+	
+	@RequestMapping(value = "/order/info", method = RequestMethod.GET)
+	public Object info(HttpServletRequest request, Long order_id) {
+		Order order = orderService.getOrder(order_id);
+		Result result = new Result();
+		result.setData(order);
 		return result;
 	}
 }
