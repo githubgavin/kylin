32565d89dc21e90124f50837af42fb5689d84cb0
heqingpan <heqingpan@126.com>
2015-07-07 15:09:39 +0800

    kylin api 添加GET /dorm/info 获取楼主信息接口

---
+++ kylin-api/src/main/java/com/store59/kylin/api/controller/DormController.java
@@ -0,0 +1,26 @@
+package com.store59.kylin.api.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store59.kylin.api.viewmodel.Result;
+import com.store59.kylin.dorm.data.model.Dorm;
+import com.store59.kylin.dorm.service.DormService;
+
+@RestController
+public class DormController {
+	@Autowired
+	private DormService dormService;
+	
+	@RequestMapping(value = "/dorm/info", method = RequestMethod.GET)
+	public Object info(HttpServletRequest request, Integer dorm_id) {
+		Dorm dorm = dormService.getDorm(dorm_id);
+		Result result = new Result();
+		result.setData(dorm);
+		return result;
+	}
+}
