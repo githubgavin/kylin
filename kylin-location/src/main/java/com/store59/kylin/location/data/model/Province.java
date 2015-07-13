2f633690a37135d43b18928bbb90e5955a95e7a4
heqingpan <heqingpan@126.com>
2015-07-13 14:47:19 +0800

    添加地理位置模块获取省份列表的逻辑

---
+++ kylin-location/src/main/java/com/store59/kylin/location/data/model/Province.java
@@ -0,0 +1,20 @@
+package com.store59.kylin.location.data.model;
+
+public class Province {
+	private Integer provinceId;
+	private String name;
+
+	public Integer getProvinceId() {
+		return provinceId;
+	}
+	public void setProvinceId(Integer provinceId) {
+		this.provinceId = provinceId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
