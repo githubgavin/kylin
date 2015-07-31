cf738237f2d7e22c15256da98530ed2dbb9815ae
陈莹波 <chenyb@59store.com>
2015-07-31 17:00:27 +0800

    add transaction support

--- kylin-datasource/src/main/java/com/store59/kylin/datasource/factory/SlaveDB.java
+++ kylin-datasource/src/main/java/com/store59/kylin/datasource/factory/SlaveDB.java
@@ -5,28 +5,49 @@ import javax.sql.DataSource;
 import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SlaveDB {
+@Configuration
+public class SlaveDB implements InitializingBean {
 	private String url = "jdbc:mysql://%s:%d/%s?useUnicode=true&characterEncoding=utf8";
 	private String driver = "com.mysql.jdbc.Driver";
 	private DataSource dataSource;
 	private SqlSessionFactoryBean sqlSessionFactory;
 	private SqlSessionTemplate sqlSession;
+	private @Value("${datasource.slave.host}") String host;
+	private @Value("${datasource.slave.port}") int port;
+	private @Value("${datasource.slave.db}") String db;
+	private @Value("${datasource.slave.username}") String username;
+	private @Value("${datasource.slave.password}") String password;
+	private @Value("${datasource.slave.maxconn}") int poolMaximumActiveConnections;
+	private @Value("${datasource.slave.minconn}") int poolMaximumIdleConnections;
 
 	@Autowired
-	public SlaveDB(
-			@Value("${datasource.slave.host}") String host,
-			@Value("${datasource.slave.port}") int port,
-			@Value("${datasource.slave.db}") String db,
-			@Value("${datasource.slave.username}") String username,
-			@Value("${datasource.slave.password}") String password,
-			@Value("${datasource.slave.maxconn}") int poolMaximumActiveConnections,
-			@Value("${datasource.slave.minconn}") int poolMaximumIdleConnections)
-			throws Exception {
+	public SlaveDB() {}
+
+	public SqlSessionTemplate getSqlSession() {
+		return this.sqlSession;
+	}
+
+	public SqlSessionFactoryBean getSqlSessionFactory() {return this.sqlSessionFactory;}
+
+	@Bean
+	SqlSessionTemplate slaveSqlSessionTemplate() {
+		return this.getSqlSession();
+	}
+
+	@Bean SqlSessionFactoryBean slaveSqlSessionFactoryBean() {
+		return this.getSqlSessionFactory();
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
 		PooledDataSource dataSource = new PooledDataSource(this.driver,
 				String.format(this.url, host, port, db), username, password);
 		dataSource
@@ -42,10 +63,4 @@ public class SlaveDB {
 		this.sqlSession = new SqlSessionTemplate(
 				this.sqlSessionFactory.getObject());
 	}
-
-	public SqlSessionTemplate getSqlSession() {
-		return this.sqlSession;
-	}
-
-	public SqlSessionFactoryBean getSqlSessionFactory() {return this.sqlSessionFactory;}
 }
