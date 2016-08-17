7ca9fbb3b27304f0c19eaff5fcdce510ec1d8fed
祝志敏 <zhuzm@59store.com>
2016-08-17 17:10:10 +0800

    add AtlasMetricObserverConfiguration

---
+++ kylin-monitor/src/main/java/com/store59/kylin/monitor/atals/AtlasMetricObserverConfiguration.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2016, 59store. All rights reserved.
+ */
+package com.store59.kylin.monitor.atals;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.regex.Pattern;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.netflix.metrics.atlas.AtlasMetricObserver;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configures the Atlas metrics observer.
+ * 
+ * @author <a href="mailto:zhuzm@59store.com">天河</a>
+ * @version 1.0 2016年8月17日
+ * @since 1.0
+ */
+@Configuration
+@ConditionalOnClass(AtlasMetricObserver.class)
+public class AtlasMetricObserverConfiguration {
+    private static final Logger logger = LoggerFactory.getLogger(AtlasMetricObserverConfiguration.class);
+
+    /**
+     * 修改AtlasMetricObserver类私有静态属性validAtlasTag.
+     * 
+     * @param observer {@link AtlasMetricObserver}
+     */
+    @Autowired
+    public void updateAtlasMetricObserver(AtlasMetricObserver observer) {
+        if (observer == null) {
+            return;
+        }
+
+        try {
+            Field field = AtlasMetricObserver.class.getDeclaredField("validAtlasTag");
+            field.setAccessible(true);
+
+            Field modifiersField = Field.class.getDeclaredField("modifiers");
+            modifiersField.setAccessible(true);
+            modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+
+            field.set(null, Pattern.compile(".*"));
+        } catch (Exception e) {
+            logger.error("修改AtlasMetricObserver类私有静态属性validAtlasTag异常", e);
+        }
+    }
+
+}
