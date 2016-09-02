59b7f4218f81d8c5526d8bda39e4a6dd840f4258
陈莹波 <chenyb@59store.com>
2016-09-02 15:54:33 +0800

    增加注释

--- kylin-rpc-parent/kylin-rpc-context/src/main/java/com/store59/kylin/rpc/context/annotation/Remote.java
+++ kylin-rpc-parent/kylin-rpc-context/src/main/java/com/store59/kylin/rpc/context/annotation/Remote.java
@@ -11,7 +11,22 @@ import java.lang.annotation.*;
 /**
  * 远程服务接口注解, 用于定义rpc默认配置, 以及申明rpc
  *
- * 使用方法详见 {@link RemoteConfiguration}
+ * 使用方法例如:
+ *
+ * @Remote
+ * public interface MsgService {
+ *   List<Message> getAllMsgList();
+ *   ...
+ * }
+ *
+ * 也可以设置接口建议默认超时时间, 该设置用于客户端不指定情况下默认使用;
+ * path配置服务暴露路径, 缺省为接口全路径名(例如: /com.store59.examplerpc.MsgService)
+ *
+ * @Remote(path="/msg", connectTimeout=2000, readTimeout=1500)
+ * public interface MsgService {
+ *   List<Message> getAllMsgList();
+ *   ...
+ * }
  *
  * @author <a href="mailto:chenyb@59store.com">山人</a>
  * @version 2.1.1 16/11/16
--- kylin-rpc-parent/kylin-rpc-context/src/main/java/com/store59/kylin/rpc/context/annotation/RemoteResource.java
+++ kylin-rpc-parent/kylin-rpc-context/src/main/java/com/store59/kylin/rpc/context/annotation/RemoteResource.java
@@ -11,6 +11,34 @@ import java.lang.annotation.*;
 /**
  * 远程服务client自动注入注解
  *
+ * 使用例如:
+ *
+ * @Service
+ * public class XXXServiceImpl {
+ *
+ *     @RemoteResource
+ *     private OrderService orderService;
+ *
+ *     @RemoteResource(connectTimeout = 2000, readTimeout = 1000)
+ *     private userService userService;
+ *
+ *     @RemoteResource
+ *     private userService userServiceShorter;
+ *
+ *     public boolean XXXX() {
+ *         ...
+ *         orderService.createOrder(...);
+ *         ...
+ *         User user = userService.findUserById(20);
+ *         ...
+ *         userServiceShorter.add(..);
+ *         ...
+ *     }
+ *
+ * }
+ *
+ * 注意: @RemoteResource中保留字段path用于兼容老版本@RemoteService做服务端的场景. 如果服务的接口上定义了@Remote注解, 则这里配置path无效.
+ *
  * @author <a href="mailto:chenyb@59store.com">山人</a>
  * @version 2.1.1 16/11/16
  * @since 2.1.1
